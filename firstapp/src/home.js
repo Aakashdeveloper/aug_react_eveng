2e7d2aae16458c2cc66ea6074a175982dbcddb0b
aakashDeveloper <ahanda205@gmail.com>
2019-08-22 18:01:35 +0100

    a

---
+++ firstapp/src/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <hr/>
+            <center>
+                <h2>Home</h2>
+            </center>
+            <hr/>
+        </div>
+    )
+}
+
+export default Header;
