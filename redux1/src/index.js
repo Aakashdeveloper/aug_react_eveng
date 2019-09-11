527256b27690e721344d6ddacb1d902b8f51f9b9
aakashDeveloper <ahanda205@gmail.com>
2019-09-11 18:16:31 +0100

    a

---
+++ redux1/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux';
+import store from './store';
+import App from './component/app.js';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+
